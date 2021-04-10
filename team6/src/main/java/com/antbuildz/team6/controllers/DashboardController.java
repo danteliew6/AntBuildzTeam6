2ddf106a2e46f63b3c85eadabf6fa6655e051258
danteliew6 <danteliew6@gmail.com>
2021-04-11 02:32:33 +0800

    updates

--- team6/src/main/java/com/antbuildz/team6/controllers/DashboardController.java
+++ team6/src/main/java/com/antbuildz/team6/controllers/DashboardController.java
@@ -1,16 +1,27 @@
 package com.antbuildz.team6.controllers;
 
+import com.antbuildz.team6.models.Request;
+import com.antbuildz.team6.repositories.RequestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
 
 @Controller
 public class DashboardController {
+    @Autowired
+    RequestRepository requestRepository;
+
     @GetMapping("/index")
     public String loginPage(Model model){
         //return the webpage for login
-        model.addAttribute("name", "your mother");
+//        ArrayList<Request> requestList = (ArrayList<Request>) requestRepository.findAll();
+//        model.addAttribute("requestList", requestList);
         return "login";
     }
 
