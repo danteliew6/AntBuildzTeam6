c56ff59095521902ebf624bf92b643310c860d72
zeph1997 <zeph1997@hotmail.com>
2021-04-13 09:45:29 +0800

    changed incremenat generationtype from auto to indentity

--- team6/src/main/java/com/antbuildz/team6/models/Bid.java
+++ team6/src/main/java/com/antbuildz/team6/models/Bid.java
@@ -12,7 +12,7 @@ import java.util.Date;
 @Table(name = "bid")
 public class Bid {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @ManyToOne
--- team6/src/main/java/com/antbuildz/team6/models/Request.java
+++ team6/src/main/java/com/antbuildz/team6/models/Request.java
@@ -10,7 +10,7 @@ import java.util.Date;
 @Table(name="request")
 public class Request {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @OneToOne
