1f6aa502e73e06cbba4c9ddabfdbd14b9a8ad6c7
zeph1997 <zeph1997@hotmail.com>
2021-04-13 11:45:51 +0800

    added query for transportrepository and removed arraylist in partner

--- team6/src/main/java/com/antbuildz/team6/models/Partner.java
+++ team6/src/main/java/com/antbuildz/team6/models/Partner.java
@@ -1,11 +1,20 @@
 package com.antbuildz.team6.models;
 
 
+import com.antbuildz.team6.repositories.TransportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import javax.persistence.Entity;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @Entity
 public class Partner extends User {
+
+    @Autowired
+    TransportRepository transportRepository;
+
     public Partner(String email, String password, String uenNumber, String companyName) {
         super(email, password, uenNumber, companyName);
     }
@@ -13,13 +22,13 @@ public class Partner extends User {
     public Partner() {
     }
 
-    private ArrayList<Transport> transports;
-
-    public ArrayList<Transport> getTransports() {
-        return transports;
-    }
-
-    public void setTransports(ArrayList<Transport> transports) {
-        this.transports = transports;
+    public Map<String,Object> getPartnerDetails(){
+        Map<String,Object> partnerDetails = super.getUserDetails();
+        ArrayList<String> transportSerialNums = new ArrayList<>();
+        for(Transport t : transportRepository.findPartnerTransports(this.getEmail())){
+            transportSerialNums.add(t.getSerialNumber());
+        }
+        partnerDetails.put("transports",transportSerialNums);
+        return partnerDetails;
     }
 }
--- team6/src/main/java/com/antbuildz/team6/repositories/TransportRepository.java
+++ team6/src/main/java/com/antbuildz/team6/repositories/TransportRepository.java
@@ -1,7 +1,14 @@
 package com.antbuildz.team6.repositories;
 
 import com.antbuildz.team6.models.Transport;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.ArrayList;
 
 public interface TransportRepository extends CrudRepository<Transport,String> {
+
+    @Query("SELECT t FROM Transport t WHERE partner_email = :email")
+    ArrayList<Transport> findPartnerTransports(@Param("email") String email);
 }
