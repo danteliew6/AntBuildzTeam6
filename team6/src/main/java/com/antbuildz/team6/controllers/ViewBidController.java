98e084c949dc34a7f8c0ffa915be400199023597
zeph1997 <zeph1997@hotmail.com>
2021-04-13 16:55:29 +0800

    added get all bids for partner

--- team6/src/main/java/com/antbuildz/team6/controllers/ViewBidController.java
+++ team6/src/main/java/com/antbuildz/team6/controllers/ViewBidController.java
@@ -1,15 +1,15 @@
 package com.antbuildz.team6.controllers;
 
 import com.antbuildz.team6.models.Bid;
+import com.antbuildz.team6.models.Partner;
 import com.antbuildz.team6.repositories.BidRepository;
+import com.antbuildz.team6.repositories.PartnerRepository;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
@@ -23,6 +23,9 @@ public class ViewBidController {
 
     @Autowired
     private BidRepository bidRepository;
+
+    @Autowired
+    private PartnerRepository partnerRepository;
 
 //    @GetMapping("/viewbid/{id}")
 //    public String bidDetails(@PathVariable String bidId, Model model){
@@ -49,5 +52,29 @@ public class ViewBidController {
         bidDetails.put("bid",bid.get().getBidDetails());
         return bidDetails;
 
+    }
+
+    @PostMapping("/viewmybids")
+    public Map<String, Object> viewPartnerBids(@RequestBody String partnerDetails){
+        // view all bids that this partner has placed
+        /*
+        {
+            "partner_email" : "hoho@gmail.com
+        }
+         */
+        JSONObject jsonObject = new JSONObject(partnerDetails);
+        Optional<Partner> partner = partnerRepository.findById(jsonObject.getString("partner_email"));
+        if(!partner.isPresent()){
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Partner not found");
+        }
+        ArrayList<Bid> bids = bidRepository.findByEmail(partner.get().getEmail());
+        Map<Integer, Map<String,Object>> bidDetails = new HashMap<>();
+        Map<String,Object> bidOutDetails = new HashMap<>();
+        for(Bid b : bids){
+            bidDetails.put(b.getId(), b.getBidDetails());
+        }
+        bidOutDetails.put("bids",bidDetails);
+        return bidOutDetails;
     }
 }
