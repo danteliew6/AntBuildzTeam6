9d5359da6246e8c324893ab7c13eca7cbb0590eb
zeph1997 <zeph1997@hotmail.com>
2021-04-14 17:25:14 +0800

    added verifyuser and get unverified users

--- team6/src/main/java/com/antbuildz/team6/repositories/UserRepository.java
+++ team6/src/main/java/com/antbuildz/team6/repositories/UserRepository.java
@@ -1,7 +1,23 @@
 package com.antbuildz.team6.repositories;
 
+import com.antbuildz.team6.models.Request;
 import com.antbuildz.team6.models.User;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
 
 public interface UserRepository extends CrudRepository<User,String> {
+
+    @Query("SELECT u FROM User u WHERE is_verified = 0")
+    ArrayList<User> findAllUnverified();
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE User SET is_verified = 1 WHERE email = :email")
+    int verifyUser(@Param("email") String email);
+
 }
